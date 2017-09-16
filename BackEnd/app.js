1b9a52364cdf928b943ab6d45051d5f74eb0270f
spinrut <32004698+spinrut@users.noreply.github.com>
2017-09-16 09:00:39 -0400

    Finished code to get user info from CapitalOne

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -67,7 +67,13 @@ User.find({ '_id': req.params.id })
 // Get user info from CapitalOne
 app.get('/getuserinfo/:id', function(req, res) {
     request.get('http://api.reimaginebanking.com/customers/' + req.params.id + '?key=' + process.env.API_KEY).end(function(response){
-        
+        if(err){
+            res.setHeader('Content-Type', 'text/html');
+            res.status(500).send({error: 'Can\'t find user info'});
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.json({firstname: res.body.first_name, lastname: res.body.last_name, address: res.body.address});
+        }
     })
 });
 
