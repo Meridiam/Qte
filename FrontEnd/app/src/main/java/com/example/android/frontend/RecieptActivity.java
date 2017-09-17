b29a6da81eb56b6083a8c21303d0fa53a418c370
Sarcatic <kw99ch@gmail.com>
2017-09-17 08:09:15 -0400

    Added a return to home button
    same as title

--- FrontEnd/app/src/main/java/com/example/android/frontend/RecieptActivity.java
+++ FrontEnd/app/src/main/java/com/example/android/frontend/RecieptActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.frontend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
+
+import static com.example.android.frontend.R.id.username;
 
 public class RecieptActivity extends AppCompatActivity {
 
@@ -26,5 +31,12 @@ public class RecieptActivity extends AppCompatActivity {
 
         TextView user = (TextView) findViewById(R.id.person_reciving_money);
         user.append(" " + stuff[stuff.length - 2]);
+        Button homeButton= (Button) findViewById(R.id.return_to_home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(RecieptActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
